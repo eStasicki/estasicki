d2710399d228f0ff3d104224faaee17d956e91f6
Ernest Stasicki <ernest.stasicki@monogo.pl>
2020-04-12 17:04:24 +0200

    add posts from json remote basic usage

---
+++ src/core/pages/HomePage/getData.js
@@ -0,0 +1,2 @@
+export const appUrl = "http://backend.estasicki.pl/wp-json/wp/v2/"
+export const getPosts = `${appUrl}posts`
--- src/core/pages/HomePage/index.js
+++ src/core/pages/HomePage/index.js
@@ -1,11 +1,39 @@
-import React from "react"
+import React, { Component } from "react"
+import { getPosts } from "./getData"
 
-const HomePage = () => (
-  <div>
-    <p>
-      To jest podstrona <strong>Strona główna</strong>
-    </p>
-  </div>
-)
+class HomePage extends Component {
+  state = {
+    posts: [],
+  }
+
+  componentDidMount() {
+    let postUrl = `${getPosts}`
+    fetch(postUrl)
+      .then((data) => data.json())
+      .then((data) => {
+        this.setState({
+          posts: data,
+        })
+      })
+  }
+
+  render() {
+    let listposts = this.state.posts.map((post, index) => {
+      return (
+        <div key={index}>
+          <h4>{post.title.rendered}</h4>
+          <p dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+        </div>
+      )
+    })
+
+    return (
+      <article>
+        <h1>Posts</h1>
+        {listposts}
+      </article>
+    )
+  }
+}
 
 export default HomePage
